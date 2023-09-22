6525c5541f221ede97be4d036f37addd088466f6
Wesley-SdS <wesleysantos.0095@gmail.com>
2023-09-22 17:49:57 -0300

    estruturando components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPricingComponent } from './components/card/card-pricing/card-pricing.component';
+import { BannerHomeComponent } from './components/banner-home/banner-home.component';
+import { HeroComponent } from './components/banner-home/hero/hero.component';
+import { AboutHeroComponent } from './components/banner-home/about-hero/about-hero.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    MenuBarComponent,
+    CardLabelComponent,
+    CardPricingComponent,
+    BannerHomeComponent,
+    HeroComponent,
+    AboutHeroComponent,
+    FooterComponent,
+ 
   ],
   imports: [
     BrowserModule,
